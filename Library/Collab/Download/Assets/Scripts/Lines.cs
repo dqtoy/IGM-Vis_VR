41552cb1f5718d7cb45e751155e2242a7733a5c9
inferrinizzard <inferrinizzard@gmail.com>
2019-10-05 14:29:55 -0700

    Lib

---
+++ Library/Collab/Download/Assets/Scripts/Lines.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using DigitalRuby.FastLineRenderer;
+
+
+[System.Serializable]
+public class Lines : MonoBehaviour
+{
+    [SerializeField] FastLineRenderer LineRenderer;
+    [SerializeField] bool useUnity = true;
+    [SerializeField] Transform lines;
+    [SerializeField] Material white;
+    [SerializeField] float radius = 1;
+    // Start is called before the first frame update
+    public void Filaments(List<Spawn.Galaxy> galaxies)
+    
+    {
+        FastLineRendererProperties props = new FastLineRendererProperties();
+        props.GlowIntensityMultiplier = 0.5f;
+        props.Radius = .01f;
+        props.Color = UnityEngine.Color.cyan;
+                    
+        // foreach(Spawn.Galaxy g in galaxies){
+		// 	Vector3 pos = g.pos;
+		// 	List<Vector3> neighbours = new List<Vector3>();
+		// 	foreach(Spawn.Galaxy other in galaxies)
+		// 		if(!other.nsaid.Equals(g.nsaid))
+		// 			if((pos-other.pos).sqrMagnitude<radius*radius)
+		// 				neighbours.Add(other.pos);
+		// 	foreach(Vector3 v in neighbours){
+        //         if(neighbours.Count>2)
+        //         if(useUnity){
+        //             GameObject lrgo = new GameObject();
+        //             LineRenderer lr = lrgo.AddComponent<LineRenderer>();
+        //             lr.widthMultiplier = .01f;
+        //             lr.SetPosition(0, pos);
+        //             lr.SetPosition(1, v);
+        //             lr.material = white;
+        //             lrgo.transform.SetParent(lines);
+        //             }
+        //         else{
+        //             props.Start = v;
+        //             props.End = pos;
+        //             LineRenderer.AddLine(props);
+        //             // LineRenderer.AppendLine(props);
+        //         }
+		// 	}
+        //     if(!useUnity)
+        //         LineRenderer.Apply();
+
+
+        //     // Collider[] neighbours = Physics.OverlapSphere(g.position, radius, 1<<9);
+        //     // Debug.Log(neighbours.Length > 0 ? g.name : "0");
+        //     // foreach(Collider c in neighbours){
+        //     //     GameObject lrgo = new GameObject();
+        //     //     lrgo.transform.SetParent(g.transform);
+        //     //     LineRenderer lr = lrgo.AddComponent<LineRenderer>();
+        //     //     lr.widthMultiplier = .1f;
+        //     //     lr.SetPosition(0, g.position);
+        //     //     lr.SetPosition(1, c.transform.position);
+        //     //     lr.material = white;
+        //     //     lr.transform.SetParent(c.transform);
+        //     // }
+        // }
+        foreach(KeyValuePair<int, List<Spawn.Galaxy>> kvp in Spawn.filaments){
+            List<Spawn.Galaxy> draw = new List<Spawn.Galaxy>();
+            draw = kvp.Value.OrderBy(f=>f.pos.sqrMagnitude).ToList();
+            
+            for(int i=0;i<draw.Count-1;i++){
+                props.Start = draw[i].pos;
+                props.End = draw[i+1].pos;
+                LineRenderer.AppendLine(props);
+                }
+                LineRenderer.Apply();
+        }
+    }
+}
---
+++ Library/Collab/Download/Assets/Scripts/Spawn.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+[System.Serializable]
+
+public class Spawn : MonoBehaviour
+{
+	string line;
+	[SerializeField] bool spawn = true, lines = false, bigSet = false;
+	[SerializeField] float scale = 1f;
+	[SerializeField] GameObject GW, ps, UI;
+	[SerializeField] Material red, blue;
+	List<Galaxy> galaxies = new List<Galaxy>();
+	public static Dictionary<int, List<Galaxy>> filaments = new Dictionary<int, List<Galaxy>>();
+
+	public struct Galaxy
+	{
+		public int nsaid;
+		public float rho, theta, phi;
+		public float size;
+		public Vector3 pos;
+		public Color color;
+		public string mstars, sfr, log_sfr, sfr_err;
+		public int filament;
+
+		public Galaxy(int id, float r, float t, float p, float s, Vector3 v, Color c, string m, string rate, string logRate, string rateError, int f)
+		{
+			nsaid = id;
+			rho = r;
+			theta = t;
+			phi = p;
+			size = s;
+			pos = v;
+			color = c;
+			mstars = m;
+			sfr = rate;
+			log_sfr = logRate;
+			sfr_err = rateError;
+			filament = f;
+		}
+	}
+
+	float parse(string s) { return float.Parse(s, CultureInfo.InvariantCulture.NumberFormat); }
+	void Start()
+	{
+		System.IO.StreamReader file = new System.IO.StreamReader(@"./Assets/galaxies" + (bigSet ? "2" :"") + ".txt");
+		while ((line = file.ReadLine()) != null)
+		{
+			string[] data = line.Split(' ');
+			if (data[0] == "NSAID")
+				continue;
+			float theta = parse(data[1]) * Mathf.PI / 180.0f;
+			float phi = parse(data[2]) * Mathf.PI / 180.0f;
+			float rho = parse(data[3]) * 1000;
+			float size = Mathf.Log(parse(data[7])) * scale;
+			int fil = (int)parse(data[10]);
+			Vector3 coords = new Vector3(rho * Mathf.Cos(theta) * Mathf.Sin(phi), rho * Mathf.Sin(theta) * Mathf.Cos(phi), rho * Mathf.Cos(phi));
+			Galaxy g = new Galaxy((int)parse(data[0]), rho, theta, phi, size, coords,
+			(data[9] == "blue" ? Color.blue : Color.red),
+			data[4],
+			data[5],
+			data[8],
+			data[6], fil);
+			// Debug.Log(coords);
+			galaxies.Add(g);
+			bool hasKey = false;
+			foreach(int k in filaments.Keys)
+				if(k == fil){
+					filaments[k].Add(g);
+					hasKey = true;}
+			if(!hasKey){
+				filaments.Add(fil,new List<Galaxy>(){g});
+				}
+			
+		}
+
+		if (spawn)
+			foreach (Galaxy g in galaxies)
+			{
+				// GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+				GameObject obj = Instantiate(ps);
+				GalaxyScript gs = obj.GetComponent<GalaxyScript>();
+				gs.data = g;
+				gs.UI = UI;
+				// obj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("images/" + g.nsaid);
+				obj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("default");
+				obj.GetComponent<SpriteRenderer>().material.color = g.color;
+				obj.SetActive(true);
+				// ParticleSystem.MainModule m = obj.GetComponent<ParticleSystem>().main;
+				// m.startColor = g.color;
+				obj.transform.SetParent(GW.transform);
+				obj.name = g.nsaid.ToString();
+				obj.transform.position = g.pos;
+				obj.transform.localScale *= g.size;
+				// obj.GetComponent<ParticleSystem>().Pause();
+				// StartCoroutine(AwaitAndPause(.5f, obj.GetComponent<ParticleSystem>()));
+				// obj.GetComponent<MeshRenderer>().material = g.color == Color.blue ? blue : red;
+			}
+
+		if(lines)
+			GetComponent<Lines>().Filaments(galaxies);
+	}
+
+	IEnumerator AwaitAndPause(float t, ParticleSystem ps)
+	{
+		yield return new WaitForSeconds(t);
+		ps.Pause();
+	}
+}
